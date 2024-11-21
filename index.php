bdb0f3b48fc2d8580d62a3f11e7c4ee35e5cb1b0
Panji Rajasa <igustipanjirajasa@gmail.com>
2024-11-21 11:54:19 +0800

    variable PHP

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Learning</title>
+</head>
+<body>
+
+</body>
+</html>
+<?php
+
+    /*variable*/
+    $user_name = "Njiee R.";
+    $hobby = "nothing";
+
+    echo "<h1>Variable in PHP</h1>";
+    echo "<p style=\" font-size: 40px; margin: 0;\">
+    Hello {$user_name}
+    </p>";
+    echo "<p style=\"font-size: 30px;\">My hobby is {$hobby}</p>";
+
+    /**/
+    
+?>
