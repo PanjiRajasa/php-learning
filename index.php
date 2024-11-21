247cc133974f81b3c3640653ce28d7c30cbe779c
Panji Rajasa <igustipanjirajasa@gmail.com>
2024-11-21 12:45:31 +0800

    GET method PHP

--- index.php
+++ index.php
@@ -4,23 +4,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Learning</title>
+
+    <style>
+        p {
+            margin: 0;
+        }
+    </style>
 </head>
 <body>
-
+    <h2>Get Method</h2>
+    <form action="index.php" method="$_GET">
+        <label>Username: </label> <br>
+        <input type="text" name="username" /> <br>
+        <label>Password: </label> <br>
+        <input type="password" name="password" /> <br> <br>
+        <input type="submit" value="Log In" />
+    </form>
+    
+    <h2>Post method</h2>
 </body>
 </html>
 <?php
-
-    /*variable*/
-    $user_name = "Njiee R.";
-    $hobby = "nothing";
-
-    echo "<h1>Variable in PHP</h1>";
-    echo "<p style=\" font-size: 40px; margin: 0;\">
-    Hello {$user_name}
-    </p>";
-    echo "<p style=\"font-size: 30px;\">My hobby is {$hobby}</p>";
-
-    /**/
     
+    /*$_GET and $_POST*/
+    echo "<p style=\"font-weight: bold;\">Hahaha! I have gotten your personal data, get ready for pinjol! </p>
+    <p>Username: {$_GET["username"]} </p>
+    <p>Password: {$_GET["password"]} </p>";
 ?>
